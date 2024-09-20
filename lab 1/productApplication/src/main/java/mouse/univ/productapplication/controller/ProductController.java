763fe3d87685091474156976e7f24f98d8736300
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-09-20 18:04:14 +0300

    add post mapping

--- lab 1/productApplication/src/main/java/mouse/univ/productapplication/controller/ProductController.java
+++ lab 1/productApplication/src/main/java/mouse/univ/productapplication/controller/ProductController.java
@@ -1,16 +1,17 @@
 package mouse.univ.productapplication.controller;
 
 import mouse.univ.productapplication.model.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products/")
 public class ProductController {
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable("id") long id) {
+        return new Product(id, id+" name");
+    }
+    @PostMapping("/{id}")
+    public Product postProduct(@PathVariable("id") long id) {
         return new Product(id, id+" name");
     }
 }
