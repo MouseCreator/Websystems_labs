723bf857a1449f5eb5f7a7e2c69573efab0f9fab
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-10-05 01:19:48 +0300

    add failing test

---
+++ lab 1/productApplication/src/main/java/mouse/univ/productapplication/component/Failing.java
@@ -0,0 +1,16 @@
+package mouse.univ.productapplication.component;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Failing {
+    @Value("${FAIL_TEST}")
+    private String val;
+    public boolean shouldFail() {
+        if (val == null || val.isEmpty()) {
+            return false;
+        }
+        return (val.equalsIgnoreCase("yes"));
+    }
+}
