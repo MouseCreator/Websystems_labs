7c3d37e5d3b9dd85084d629d429666a13b8f5401
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-10-07 22:00:24 +0300

    remove extra logs

--- lab 1/productApplication/src/main/java/mouse/univ/productapplication/component/Failing.java
+++ lab 1/productApplication/src/main/java/mouse/univ/productapplication/component/Failing.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 public class Failing {
     private final String val = System.getProperty("FAIL_TEST");
     public boolean shouldFail() {
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!" + val);
         if (val == null || val.isEmpty()) {
             return false;
         }
