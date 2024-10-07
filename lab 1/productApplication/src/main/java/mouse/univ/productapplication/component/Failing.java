ab2a4d193dd7f22ac442e974aea6be11960d2986
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-10-07 20:57:41 +0300

    add value annotation

--- lab 1/productApplication/src/main/java/mouse/univ/productapplication/component/Failing.java
+++ lab 1/productApplication/src/main/java/mouse/univ/productapplication/component/Failing.java
@@ -1,10 +1,12 @@
 package mouse.univ.productapplication.component;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Failing {
-    private final String val = System.getenv("FAIL_TEST");
+    @Value("${FAIL_TEST:no}")
+    private String val;
     public boolean shouldFail() {
         if (val == null || val.isEmpty()) {
             return false;
