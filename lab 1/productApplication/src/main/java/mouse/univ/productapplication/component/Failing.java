514c509c1cf692a04b95a282fae2f741ef99d5b1
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-10-07 21:58:14 +0300

    use system properties

--- lab 1/productApplication/src/main/java/mouse/univ/productapplication/component/Failing.java
+++ lab 1/productApplication/src/main/java/mouse/univ/productapplication/component/Failing.java
@@ -1,13 +1,12 @@
 package mouse.univ.productapplication.component;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Failing {
-    @Value("${FAIL_TEST:no}")
-    private String val;
+    private final String val = System.getProperty("FAIL_TEST");
     public boolean shouldFail() {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!" + val);
         if (val == null || val.isEmpty()) {
             return false;
         }
