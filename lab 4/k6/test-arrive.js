77ed42e88b976693aeb52d882692c11e16a61614
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-11-04 00:21:53 +0200

    add scenarios

---
+++ lab 4/k6/test-arrive.js
@@ -0,0 +1,33 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+
+
+function randomIntBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const options = {
+	scenarios: {
+		products: {
+			executor: 'constant-arrival-rate',
+			duration: '1m',
+			rate: 1,
+			preAllocatedVUs: 5,
+			timeUnit: '5s',
+			maxVUs: 40
+		}
+	}
+}
+
+export default function() {
+	const productId = randomIntBetween(1, 1000)
+	const timeout = randomIntBetween(1, 150)
+	const url = `http://spring-boot-app:8080/products/${productId}?timeout={timeout}`
+	const response = http.get(url)
+	
+	check(response, {
+		'status': (r) => r.status === 200
+	})
+	
+	sleep(0.25);
+}
---
+++ lab 4/k6/test-think.js
@@ -0,0 +1,34 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+
+
+function randomIntBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function thinkTime() {
+  return randomIntBetween(1, 4);
+}
+
+export const options = {
+	scenarios: {
+		products: {
+			executor: 'constant-vus',
+			duration: '1m',
+			vus: 20
+		}
+	}
+}
+
+export default function() {
+	const productId = randomIntBetween(1, 1000)
+	const timeout = randomIntBetween(1, 150)
+	const url = `http://spring-boot-app:8080/products/${productId}?timeout={timeout}`
+	const response = http.get(url)
+	
+	check(response, {
+		'status': (r) => r.status === 200
+	})
+	
+	sleep(thinkTime());
+}
